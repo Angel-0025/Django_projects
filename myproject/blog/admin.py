6516b1497c9fd52c787b36ae6bd24083869a77a8
Angel Oribiada <oribiada24@gmail.com>
2023-04-22 17:52:05 +0800

    day 3: models/database and display data

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Blog_User, Post
 
-# Register your models here.
+
+admin.site.register(Blog_User)
+admin.site.register(Post)
--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog_User(models.Model):
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=75)
+    
+    def __str__(self):
+        return self.username
+    
+class Post(models.Model):
+    title = models.TextField(max_length=50)
+    content = models.TextField(max_length=150)
+    author = models.ForeignKey(Blog_User, on_delete=models.CASCADE)
+    date_posted = models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
